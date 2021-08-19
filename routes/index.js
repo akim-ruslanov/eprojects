3dad14801aaa2f02e3c4e394da2975b3b5744159
aruslanov@fispan.com <aruslanov@fispan.com>
2021-08-18 22:20:56 -0700

    alpha version for viewing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'eProjects', main_page: true });
 });
+
+
+// TODO: pull data from somewhere
+router.get('/blog', function(req, res, next) {
+  res.render('blog', {blogInfo: 'NONE', title:"Blog", main_page: false})
+})
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {blogInfo: 'NONE', title:"Contact", main_page: false})
+})
+
+
+router.get('/news', function(req, res, next) {
+  res.render('newsletter', {blogInfo: 'NONE', title:"Newsletter", main_page: false})
+})
 
 module.exports = router;
